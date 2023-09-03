ba765ad47dbb0e2cd16febc5e687d57ac6c92c02
pablorgarcia <pablo.ramon.garcia.fernandez@gmail.com>
2023-09-03 18:44:24 +0200

    improve interface

---
+++ src/interfaces/console.ts
@@ -0,0 +1,13 @@
+export interface Console {
+  name: string;
+  accessoryConsoleId: string; 
+  companyId: string;
+  cost: number;
+  count: number;
+  description: string;
+  image: string;
+  mods: string;
+  regionId: string;
+  countryId: string;
+  serialNumber: string;
+}
---
+++ src/interfaces/region.ts
@@ -0,0 +1,5 @@
+export interface Region {
+  NTSC: string[];
+  PAL: string[];
+  Regionfree: string[];
+}
